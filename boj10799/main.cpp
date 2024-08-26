c99d3947179de8d1d1a5e96446f6e359e60fd490
fixedperson <fixedperson1@gmail.com>
2024-08-26 15:05:42 +0900

    boj

--- boj10799/main.cpp
+++ boj10799/main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main()
+{
+    string batch;
+    cin >> batch;
+
+    int sum = 0;
+    int stick = 0;
+    for(int i = 0; i < batch.size(); i++) {
+        if(batch[i] == '(') {
+            stick++;
+        }
+
+        else {
+            stick--;
+            if(batch[i-1] == '(') {
+                sum += stick;
+            }
+
+            else {
+                sum += 1;
+            }
+        }
+    }
+
+    cout << sum;
 }
